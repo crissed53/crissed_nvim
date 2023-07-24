fe42fac30a32621be53206aa8299604118715714
crissed <jisu.jeon53@navercorp.com>
2023-07-24 01:59:02 +0000

    ./

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,39 +1,33 @@
--- customize mason plugins
 return {
-  -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
+        "html",
+        "pyright",
         "lua_ls",
-        "pyright"
-      })
-    end,
+      },
+    },
   },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "prettier",
+    opts = {
+      ensure_installed = {
         "stylua",
         "black",
-        "ruff"
-      })
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "python",
-      })
+        "ruff",
+        "prettierd",
+      },
+    },
+    config = function(_, opts)
+      local mason_null_ls = require "mason-null-ls"
+      local null_ls = require "null-ls"
+      mason_null_ls.setup(opts)
+      mason_null_ls.setup {
+        prettierd = function()
+          null_ls.register(null_ls.builtins.formatting.prettierd.with { extra_filetypes = { "json", "markdown" } })
+        end,
+      }
     end,
   },
 }
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -7,4 +7,48 @@ return {
       "python",
     })
   end,
+  dependencies = {
+    {
+      "HiPhish/nvim-ts-rainbow2",
+      config = function()
+        require("nvim-treesitter.configs").setup {
+          rainbow = {
+            enable = true,
+            query = "rainbow-parens",
+            strategy = require "ts-rainbow.strategy.global",
+          },
+        }
+      end,
+    },
+    {
+      "Wansmer/treesj",
+      keys = "sj",
+      config = function()
+        require("astronvim.utils").set_mappings {
+          n = {
+            ["sj"] = { ":TSJToggle<CR>", desc = "Treesj toggle" },
+          },
+        }
+        require("treesj").setup {
+          use_default_keymaps = false,
+        }
+      end,
+    },
+    {
+      "Wansmer/sibling-swap.nvim",
+      keys = { "sl", "sh" },
+      opts = {
+        keymaps = {
+          ["sl"] = "swap_with_right",
+          ["sh"] = "swap_with_left",
+          -- ['sL'] = 'swap_with_right_with_opp',
+          -- ['sH'] = 'swap_with_left_with_opp',
+        },
+      },
+    },
+    {
+      "nvim-treesitter/playground",
+      cmd = "TSPlaygroundToggle",
+    },
+  },
 }
